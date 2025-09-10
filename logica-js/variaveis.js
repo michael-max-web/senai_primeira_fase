91979b421200f059e729ebc5f3fb5a7633322c97
michael-max-web <michael4671111@gmail.com>
2025-09-10 19:46:50 -0300

    javascript pra valer

--- logica-js/variaveis.js
+++ logica-js/variaveis.js
@@ -15,7 +15,7 @@
 
  // console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos ")
 
-console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
 
 //array
 const frutas = ["laranja","maçã","banana"]
