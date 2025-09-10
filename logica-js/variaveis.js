bf8dd233795c901a93cbc9f8f82d326083c0276b
michael-max-web <michael4671111@gmail.com>
2025-09-09 22:14:10 -0300

    atualizacao javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,24 @@
+// não se pode criar variável com var pq ela permite criar variáveis iguais
+// var nome = "Michael"
+// var nome = "João"
+// nome = "João"
+
+// variável com let
+// let nome = "Michael"
+// nome = "João"
+
+// variável declarada com const
+// const nome = "Michael" //string 
+// const sobrenome = "Sousa"
+// const idade = 23 //number
+// const masculino = true //boolean
+
+ // console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos ")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
+
+//array
+const frutas = ["laranja","maçã","banana"]
+console.log(fruta[0])
+
+
