dedab5c2ddd5d5665e58fe84830be008e760dac9
michael-max-web <michael4671111@gmail.com>
2025-09-10 19:30:00 -0300

    atualizado

---
+++ aula_javascript/script.js
@@ -0,0 +1,22 @@
+// alert("Olá, deseja aceitar todos os cookies?")
+// alert("Cookies aceitos. Bem vindo ao HTML!")
+
+// let idade
+
+// idade = 18
+// alert(idade)
+
+// idade = "dezoito"
+// alert(idade)
+
+// idade = 18.1
+// alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
+let amor2 = "meu segundo amor"
+alert(amor)
+
+var antigao
+
