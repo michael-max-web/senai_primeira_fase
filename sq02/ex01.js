de5fc48c8e71ca6ca6aac7219f0b715013b48e8a
michael-max-web <michael4671111@gmail.com>
2025-09-10 22:19:28 -0300

    exercicios javascript

---
+++ sq02/ex01.js
@@ -0,0 +1,14 @@
+// let idade 
+
+// idade = Number( prompt("Digite sua idade: "))
+
+// if(idade >= 18){
+//     alert("Maior de idade")
+//     alert("Pode sair pro intervalo")
+// }else{
+//     alert("Dimenó")
+//     alert("Pode sair pro recreio")
+// }
+
+
+
---
+++ sq02/ex04.js
@@ -0,0 +1,23 @@
+let numero
+
+numero = Number( prompt("Digite o número: "))
+
+if(numero % 2 == 0){
+    alert("Esse número é par")
+}else{
+    alert("Esse número é í
+        mpar")
+    
+}
+
+// *exemplo em português*
+
+// ler um número
+
+// se o número for par
+//     informar que é par
+
+// senão
+//     informar que é ímpar
+
+
