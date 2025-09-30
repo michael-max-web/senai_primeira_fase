14d5e00f8edc503e641b837a38d18c85b205079e
michael-max-web <michael4671111@gmail.com>
2025-09-29 22:16:43 -0300

    Repetições

---
+++ repeticoes/script.js
@@ -0,0 +1,13 @@
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert("Sua nota: "+ nota)
+
+let contador = 0
+// "while" equivale a "enquanto"
+while(contador < 2){
+// alert("Contador: " + contador)
+console.log("Contador:" + contador)
+contador = contador + 1
+}
