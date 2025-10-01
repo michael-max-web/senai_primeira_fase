8e260b154b9f2d5280e7f3dd5d9f2dc9bb02704b
michael-max-web <michael4671111@gmail.com>
2025-09-30 22:10:35 -0300

    Funções

---
+++ logica-js/for.js
@@ -0,0 +1,39 @@
+// for(let i = 0; i<=10; i++){
+//     console.log(i)
+// }
+
+// for(let i = 10; i>=0; i--){
+//  console.log(i)
+// }
+
+// for(let i = 0; i<=20; i+=2){
+//  console.log(i)
+// }
+
+// let soma = 0
+
+// for(let i = 1; i<100; i ++){
+//     soma += i
+// }
+
+// console.log("Resultado da Soma", soma)
+
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+// alunos[0], alunos[1] ...
+
+// console.log(alunos.length)
+
+// let alunos = ["João","Maria","Joana"]
+// alunos.push("Pedro")
+
+// for(let i = 0; i<alunos.length; i++){
+//     console.log("aluno: ", alunos[i])
+// }
+
+let frutas = ["Pera","Maçã","Uva","Melancia","Morango"]
+
+for(let i = 0; i<frutas.length; i++){
+    console.log("Frutas: ", frutas[i])
+}
---
+++ logica-js/funcoes.js
@@ -0,0 +1,36 @@
+// function saudacao(nome){
+//     console.log(`Bom dia, ${nome}`)
+
+// }
+
+// const saudacao = (nome) =>{
+//     console.log(`Bom dia, ${nome}`)
+// }
+// saudacao("Michael!")
+// saudacao("Maria!")
+
+// const soma = (x,y) => x+y
+
+// const soma = (x,y) =>{
+//     return x+y
+// }
+
+// console.log(soma(4, 5))
+
+// const multiplicacao = (x,y) =>{
+//     return x*y
+// }
+
+// console.log(multiplicacao(3, 3))
+
+// const divisao = (x,y) =>{
+//     return x/y
+// }
+
+// console.log(divisao(4, 2))
+
+const subtracao = (x,y) =>{
+    return x-y
+}
+
+console.log(subtracao(4, 1))
