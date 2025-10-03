9a042373581865ce307fb3b4827fde74778080ee
michael-max-web <michael4671111@gmail.com>
2025-10-02 21:27:17 -0300

    atualizaçãoDeRepetições

---
+++ repeticoes/for.js
@@ -0,0 +1,51 @@
+
+// let i = 0
+// while(i<3){
+//     alert("Valor do i: " + i)
+//     i++
+
+// }
+
+// for(let i = 0; i < 3; i++){
+//     alert("Valor do i : " + i)
+// }
+
+
+// let soma = 0
+// for(let i=0; i<10; i++){
+//     let nota = Number(prompt("Digite a " + (i+1) + "a nota: ")) 
+//     soma = soma + nota
+    
+// }
+
+// alert("Soma: " + soma)
+
+// const nomes = ["Dona Bete", "Mano Juca", "Lúcio Fernando", "Capitão Ganso"]
+// for(let i=0; i<4; i ++){
+//     alert(nomes[i])
+// }
+
+
+
+// ex1
+// for(let i=0; i<10; i++){
+//     let quadrado = i*i
+//     alert(quadrado)
+// }
+
+
+
+// ex2
+// for(let i=10; i>0; i--){
+//      alert(i)
+// }
+
+
+
+// ex3
+let soma = 0
+ for(let i=0; i<100; i++){
+     let nota = Number(prompt("Digite a " + (i+1) + "a nota: ")) 
+     soma = soma + nota
+    
+}
