9240adf09e71474272a0db1a1c25cc699dbdd406
michael-max-web <michael4671111@gmail.com>
2025-10-21 22:16:59 -0300

    html e css

---
+++ repeticoes/loops/ex-for.js
@@ -0,0 +1,36 @@
+//  for(let i=0; i<100; i++){
+//     console.log("Os 100 primeiros números são: ", i)
+//  }
+
+// let i = 1;
+// while (i <= 100){
+//     console.log("Os 100 primeiros números são: ", i);
+//     i++;
+// }
+let dia = 6
+switch (dia) {
+    case 1:
+        console.log("segunda-eira")
+        break;
+    case 2:
+        console.log("terça-feira")
+        break;
+    case 3:
+        console.log("quarta-feira")
+        break;
+    case 4:
+        console.log("quinta-feira")
+        break;
+    case 5:
+        console.log("sexta-feira")
+        break;
+    case 6:
+        console.log("sábado")
+        break;
+    case 7:
+        console.log("domingo")
+        break;        
+    default:
+        console.log("Opção Inválida");
+        break;
+}
--- repeticoes/parte1-4.js
+++ repeticoes/parte1-4.js
@@ -3,7 +3,7 @@
 //     console.log(i + "x" + 5 + "=" + resultado);
 // }
 
-let numero = Number(prompt("Tabuadado quê?"))
+let numero = Number(prompt("Tabuada do quê?"))
 for(let i=1; i<10; i++){
     let resultado = i * 5
     console.log(i + "x" + 5 + "=" + resultado);
